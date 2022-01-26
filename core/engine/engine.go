b08d0c63a4f909b1983ac3d606c4f7d1d392e778
wangxiaohui <wangxiaohui@hyperchain.cn>
2022-01-26 17:25:50 +0800

    feat(txnum):add txnum statistcs and modify engine run

--- core/engine/engine.go
+++ core/engine/engine.go
@@ -79,26 +79,21 @@ func (b *baseEngine) Run(callback Callback) {
 }
 
 func (b *baseEngine) schedule(callback Callback) {
-	b.timeoutCtx, b.cancelFunc = context.WithTimeout(context.Background(), b.Duration)
+	totalBatch, batchCount := int(b.Duration/b.interval), 0
 	tick := time.NewTicker(b.interval)
 	defer func() {
 		tick.Stop()
 	}()
-
-	var timeout = b.timeoutCtx
-	var i int64
-
-	for {
-		select {
-		case <-timeout.Done():
-			return
-		case <-tick.C:
-			for i = 0; i < b.batch; i++ {
+	for ; batchCount < totalBatch; batchCount++ {
+		<-tick.C
+		go func() {
+			for i := int64(0); i < b.batch; i++ {
 				b.Wg.Add(1)
 				go callback()
 			}
-		}
+		}()
 	}
+	time.Sleep(b.interval)
 }
 
 // Close close the engine.
